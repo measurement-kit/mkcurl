6104223850d8bbdac0602cceb6d2f999ffbe830c
Simone Basso <bassosimone@gmail.com>
2019-01-08 13:17:40 +0100

    No need to define MKMOCK_HOOK_ENABLE

--- unit-tests.cpp
+++ unit-tests.cpp
@@ -20,9 +20,6 @@ struct MkCurlAbort : public std::exception {
 // MKCURL_ABORT overrides the default MKCURK_ABORT to throw MkCurlAbort.
 #define MKCURL_ABORT() throw MkCurlAbort()
 
-// By setting MKMOCK_HOOK_ENABLE and including mkmock.hpp we cause mkcurl.hpp
-// to compile with mocking enabled so that we can run unit tests.
-#define MKMOCK_HOOK_ENABLE
 #include "mkmock.hpp"
 
 MKMOCK_DEFINE_HOOK(curl_easy_init, CURL *);
